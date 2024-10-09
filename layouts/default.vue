ab79e75152537c67634fbc7040526470cc7b4120
Stanislav Kozin <stas@staskozin.ru>
2024-10-09 21:14:33 +0300

    Добавил базовые стили и фавиконку

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup>
+import '~/assets/globals.css'
+
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      href: '/favicon.ico',
+      sizes: '32x32'
+    },
+    {
+      rel: 'icon',
+      href: '/icon.png',
+      sizes: '192x192'
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/apple-touch-icon.png'
+    }
+  ]
+})
+</script>
+
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
