7a31fc0bc6362273716c887727f4d738d41a78d4
Stanislav Kozin <stas@staskozin.ru>
2024-10-21 22:52:36 +0300

    Create Project.vue

---
+++ components/Project.vue
@@ -0,0 +1,31 @@
+<style scoped>
+.project {
+  display: block;
+  margin-bottom: var(--masonry-gap);
+  width: 100%;
+}
+
+.project>div {
+  margin-bottom: 4px;
+}
+
+.text {
+  line-height: 1.25;
+}
+</style>
+
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+  text: string
+}>()
+</script>
+
+<template>
+  <a class="project" :href="props.href">
+    <ResponsiveImage>
+      <slot />
+    </ResponsiveImage>
+    <span className={s.text}>{{ props.text }}</span>
+  </a>
+</template>
