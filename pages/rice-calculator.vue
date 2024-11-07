1bbff3a3200379ffdb7d0ae08b5896123a8a5255
Stanislav Kozin <stas@staskozin.ru>
2024-11-07 21:03:45 +0300

    Add basic state to rice recipe

--- pages/rice-calculator.vue
+++ pages/rice-calculator.vue
@@ -17,4 +17,26 @@ useHead({
     }
   ]
 })
+
+const rice = ref<number>(450)
+const ratio = ref<number>(1.15)
+const purpose = ref<Purpose>('sushi')
+const riceType = ref<RiceType>('round')
+const pot = ref<Pot>('pot')
+const ingredients = ref<Ingredients>({
+  water: 518,
+  vinegar: 50,
+  salt: 5,
+  sugar: 41
+})
+
+export type Purpose = 'sushi' | 'side' | 'porridge';
+export type RiceType = 'round' | 'long' | 'parboiled';
+export type Pot = 'pot' | 'multi' | 'pan';
+type Ingredients = {
+  water: number
+  vinegar: number
+  sugar: number
+  salt: number
+};
 </script>
